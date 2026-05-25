25542308593f9e6efb9ca468ded8eabbed011ef9
Guilherme <guilherme@utfpr.edu.br>
2026-05-25 00:36:40 -0300

    Projeto final de semestre

--- fix2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-['index.html', 'README.md'].forEach(file => {
-  const content = fs.readFileSync(file, 'utf8');
-  const replaced = content.replace(/assets(\/)?/g, 'imagens$1');
-  fs.writeFileSync(file, replaced);
-  console.log('Fixed', file);
-});
